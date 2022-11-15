bc8898490f34572d69ea198df65a73f149d57386
KhatrusovSN <113129616+KhatrusovSN@users.noreply.github.com>
2022-11-15 19:53:51 +0300

    Добавьте файлы проекта.

---
+++ 10.5FinalProject/Calculate.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10._5FinalProject
+{
+    internal class Calculate : ICalculate
+    {
+        ILogger Logger { get; }
+        public Calculate(ILogger logger)
+        {
+            Logger = logger;
+        }
+
+        double ICalculate.Sum(double x, double y)
+        {
+            Logger.Event("Sum : ");
+            return x + y;
+        }
+    }
+}
---
+++ 10.5FinalProject/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _10._5FinalProject
+{
+    internal class Program
+    {
+        static ILogger? Logger { get; set; }
+        
+        static void Main(string[] args)
+        {
+            Logger = new Logger();
+            Calculate calculate = new Calculate(Logger);
+            double value1, value2;
+
+            try
+            {
+                Console.Write("Value1 = ");
+                value1 = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Value2 = ");
+                value2 = Convert.ToDouble(Console.ReadLine());
+
+                double result = ((ICalculate)calculate).Sum(value1, value2);
+                Console.Write(result);
+            }
+            catch (Exception ex)
+            {
+                Logger.Error("Exception : ");
+                Console.Write(ex.Message);
+            }
+        }
+    }
+}
